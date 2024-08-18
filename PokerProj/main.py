0a02d23d868036f28a8fa837a8f685dd065a86c3
Luke Cheng <lujo.cheng12@gmail.com>
2024-08-18 16:56:14 -0700

    Update main.py

--- PokerProj/main.py
+++ PokerProj/main.py
@@ -4,19 +4,26 @@ import random
 
 
 def main():
-    p1 = []
-    c1 = random.choice(clublist)
-    clublist.remove(c1)
-    c2 = random.choice(clublist)
-    clublist.remove(c2)
-    p1.append(c1)
-    p1.append(c2)
-    
-    
+    players = int(input("Enter number of Players: "))
+    while (2 > players) or (players > 8):
+        players = int(input("Must be at least two players (max 8 players): "))
 
-    for i in (p1):
-        print(i)
-    for j in clublist:
-        print(j)
-
+    playerhands = []
+    
+    for i in range (players):
+        hand = []
+        c1 = random.choice(cardlist)
+        cardlist.remove(c1)
+        c2 = random.choice(cardlist)
+        cardlist.remove(c2)
+        hand.append(c1)
+        hand.append(c2)
+        playerhands.append(hand)
+    
+    k = 1
+    for i in playerhands: 
+        print("Hand" , str(k) + ":") 
+        for j in i: print (j)
+        k += 1
+        print('')
 main()
