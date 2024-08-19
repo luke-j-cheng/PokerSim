fe561db656e6929b8a21c8433b6f9623f0ec494a
Luke Cheng <lujo.cheng12@gmail.com>
2024-08-19 16:42:35 -0700

    Updated nutsearch

--- PokerProj/Nutsearch.py
+++ PokerProj/Nutsearch.py
@@ -6,78 +6,89 @@ from cards import *
 
 def StraightFlush(list):
     if True:
-        return True
+        return (True, score)
     else:
         return False
 
 def Quads(list):
     if True:
-        return True
+        return (True, score)
     else:
-        return False
+        return (False, None)
 
 def FullHouse(list):
     if True:
-        return True
+        return (True, score)
     else:
-        return False
+        return (False, None)
 
 def Flush(list):
     if True:
-        return True
+        return (True, score)
     else:
-        return False
+        return (False, None)
 
 def Straight(list):
     if True:
-        return True
+        return (True, score)
     else:
-        return False
+        return (False, None)
 
 def Set(list):
     if True:
-        return True
+        return (True, score)
     else:
-        return False
+        return (False, None)
 
-def TwoPair(list):
-    if True:
-        return True
-    else:
-        return False
+
 
 def Pair(list):
-    if True:
-        return True
-    else:
-        return False
+    paircheck = []
+    paircount = 0
+    highest = []
+    for i in list:
+        if i not in paircheck:
+            paircheck.append(i)
+        else:
+            paircount += 1
+            highest.append(i)
+    if paircount > 2:
+        paircount = 2
 
-def HighCard(list):
-    highest = 1
-    return highest
+    
+    
+    
+    if paircount != 0:
+        return (True, paircount, highest)
+    else:
+        return (False, None)
+
+def HighCard(list): #Will be used to break ties by entering modified hands 
+    highcard = []
+    for card in list:
+        highcard.append(card.value)
+    highest = highcard.sort(reverse=True)
+    return (True, highest)
     
 
-
+#Nutsearch returns a list - first value is the score of the hand, second is the value of the score (i.e. for a pair of Nines, value is [2, 9])
 def nutsearch(list):
-    if StraightFlush(list):
+    if StraightFlush(list)[0]:
         pass
-    elif Quads(list):
+    elif Quads(list)[0]:
         pass
-    elif FullHouse(list):
+    elif FullHouse(list)[0]:
         pass
-    elif Flush(list):
+    elif Flush(list)[0]:
         pass
-    elif Straight(list):
+    elif Straight(list)[0]:
         pass
-    elif Set(list):
+    elif Set(list)[0]:
         pass
-    elif TwoPair(list):
-        pass
-    elif Pair(list):
+    elif Pair(list)[0]:
         pass
     else:
-        HighCard(list)
-
+        return (0, HighCard(list)[1])
 
 
 
